8b72ca82a3951fe93b5fcb6dbe0597ed15430aa2
Yuzhong23 <xixixixifatur@gmail.com>
2024-05-14 11:00:26 +0700

    Menyelesaikan kodingan belajar static lewat fuction

--- Constructor_0157 (2)/Constructor_0157 (2).cpp
+++ Constructor_0157 (2)/Constructor_0157 (2).cpp
@@ -2,7 +2,45 @@
 #include <string>
 using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+class mahasiswa {
+private:
+	static int nim;
+public:
+	int id;
+	string nama;
+
+	void setID();
+	void printAll();
+
+	static void setNim(int pNim) { nim = pNim; }
+	static int getNim() { return nim; }
+
+	mahasiswa(string pnama) :nama(pnama) { setID(); }
+};
+
+int mahasiswa::nim = 0;
+
+void mahasiswa::printAll() {
+	cout << "ID = " << id << endl;
+	cout << "Nama =" << nama << endl;
+	cout << endl;
+}
+
+int main() {
+
+	mahasiswa mhs1("Sri Dadi");
+	mahasiswa mhs2("Budi Jatmiko");
+	mahasiswa::setNim(9);
+	mahasiswa mhs3("Andi Janu");
+	mahasiswa mhs4("Joko Wahono");
+
+	mhs1.printAll();
+	mhs2.printAll();
+	mhs3.printAll();
+	mhs4.printAll();
+
+	cout << "akses dari luar object = " << mahasiswa::getNim() << endl;
+	system("pause");
+
+	return 0;
 }
